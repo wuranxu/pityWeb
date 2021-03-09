92317a9073f54bb62ebb1472eee3d2cf99291887
wuranxu <wuranxu@126.com>
2021-03-09 22:46:26 +0800

    初始化pityWeb项目

---
+++ src/components/Authorized/renderAuthorize.js
@@ -0,0 +1,31 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+
+/* eslint-disable import/no-mutable-exports */
+let CURRENT = 'NULL';
+
+/**
+ * Use authority or getAuthority
+ *
+ * @param {string|()=>String} currentAuthority
+ */
+const renderAuthorize = (Authorized) => (currentAuthority) => {
+  if (currentAuthority) {
+    if (typeof currentAuthority === 'function') {
+      CURRENT = currentAuthority();
+    }
+
+    if (
+      Object.prototype.toString.call(currentAuthority) === '[object String]' ||
+      Array.isArray(currentAuthority)
+    ) {
+      CURRENT = currentAuthority;
+    }
+  } else {
+    CURRENT = 'NULL';
+  }
+
+  return Authorized;
+};
+
+export { CURRENT };
+export default (Authorized) => renderAuthorize(Authorized);
