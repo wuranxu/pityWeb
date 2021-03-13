68e176eb723d477f912084e1a9c6b5c2fa1343a2
wuranxu <wuranxu@126.com>
2021-03-14 00:34:36 +0800

    新增可编辑table

--- src/components/Postman/Postman.jsx
+++ src/components/Postman/Postman.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Card, Col, Row, Input, Select, Button, Tabs, Radio, Dropdown, Menu } from 'antd';
-import { DownOutlined, SendOutlined } from '@ant-design/icons';
+import { DownOutlined, SendOutlined, DeleteTwoTone, EditTwoTone } from '@ant-design/icons';
+import EditableTable from '@/components/Table/EditableTable';
 
 const { Option } = Select;
 const { TabPane } = Tabs;
 
 const selectBefore = (
-  <Select defaultValue='GET' style={{ width: 120, fontSize: 16, fontWeight: 400, textAlign: 'left' }}>
+  <Select defaultValue='GET' style={{ width: 120, fontSize: 16, textAlign: 'left' }}>
     <Option value='GET'>GET</Option>
     <Option value='POST'>POST</Option>
     <Option value='PUT'>PUT</Option>
@@ -19,9 +20,55 @@ const selectBefore = (
 export default () => {
   const [bodyType, setBodyType] = useState('none');
   const [rawType, setRawType] = useState('JSON')
+  const [paramsData, setParamsData] = useState([]);
+  const [editableKeys, setEditableRowKeys] = useState(() => paramsData.map((item) => item.id));
+
+  // 请求url+params
+  const [url, setUrl] = useState('');
+
+  // 根据paramsData拼接url
+  const joinUrl = (data) => {
+    let tempUrl = url.split('?')[0];
+    data.forEach((item, idx) => {
+      if (item.key) {
+        // 如果item.key有效
+        if (idx === 0) {
+          tempUrl = `${tempUrl}?${item.key}=${item.value || ''}`;
+        } else {
+          tempUrl = `${tempUrl}&${item.key}=${item.value || ''}`;
+        }
+      }
+    })
+    setUrl(tempUrl);
+  }
+
+  const splitUrl = nowUrl => {
+    const split = nowUrl.split('?')
+    if (split.length < 2) {
+      setParamsData([]);
+    } else {
+      const params = split[1].split("&")
+      const newParams = [];
+      const keys = [];
+      params.forEach((item, idx) => {
+        const [key, value] = item.split("=");
+        const now = Date.now();
+        keys.push(now+idx+10);
+        newParams.push({key, value, id: now+idx+10, description: ''})
+      })
+      setParamsData(newParams)
+      setEditableRowKeys(keys);
+    }
+  }
 
   const onClickMenu = key => {
     setRawType(key);
+  }
+
+  const onDelete = key => {
+    const data = paramsData.filter(item => item.id !== key)
+    setParamsData(data);
+    joinUrl(data);
   }
 
   const menu = (
@@ -45,20 +92,48 @@ export default () => {
     </Menu>
   );
 
+  const paramsColumns = [
+    {
+      title: 'KEY',
+      dataIndex: 'key',
+    },
+    {
+      title: 'VALUE',
+      dataIndex: 'value',
+    },
+    {
+      title: 'DESCRIPTION',
+      dataIndex: 'description',
+    },
+    {
+      title: '操作',
+      render: (text, record) => {
+        return <>
+          <DeleteTwoTone style={{cursor: 'pointer', marginLeft: 8}} onClick={()=>{onDelete(record.id)}} twoToneColor="#eb2f96"/>
+        </>
+      }
+    },
+  ]
+
   return (
     <Card>
       <Row gutter={[8, 8]}>
         <Col span={18}>
-          <Input size='large' addonBefore={selectBefore} defaultValue='mysite' />
+          <Input size='large' value={url} addonBefore={selectBefore} placeholder="请输入要请求的url" onChange={ e => {
+            setUrl(e.target.value)
+            splitUrl(e.target.value);
+          }} />
         </Col>
         <Col span={6}>
           <Button type='primary' size='large' style={{ marginRight: 16, float: 'right' }}><SendOutlined />Send </Button>
         </Col>
       </Row>
       <Row style={{ marginTop: 8 }}>
-        <Tabs defaultActiveKey='1'>
+        <Tabs defaultActiveKey='1' style={{width: '100%'}}>
           <TabPane tab='Params' key='1'>
-            这是params
+            <EditableTable columns={paramsColumns} title="Query Params" dataSource={paramsData} setDataSource={setParamsData}
+            extra={joinUrl} editableKeys={editableKeys} setEditableRowKeys={setEditableRowKeys}
+            />
           </TabPane>
           <TabPane tab='Headers' key='2'>
             这是Headers
---
+++ src/components/Table/EditableTable.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { EditableProTable } from '@ant-design/pro-table';
+
+export default ({columns, dataSource, title, setDataSource, editableKeys, setEditableRowKeys, extra}) => {
+  return (
+    <EditableProTable headerTitle={title} columns={columns} rowKey="id" value={dataSource} onChange={setDataSource}
+                      recordCreatorProps={{
+                        newRecordType: 'dataSource',
+                        record: () => ({
+                          id: Date.now(),
+                        }),
+    }}  editable={{
+      type: 'multiple',
+      editableKeys,
+      actionRender: (row, config, defaultDoms) => {
+        return [defaultDoms.delete];
+      },
+      onValuesChange: (record, recordList) => {
+        extra(recordList);
+        setDataSource(recordList);
+      },
+      onChange: setEditableRowKeys,
+    }}/>
+  )
+}
